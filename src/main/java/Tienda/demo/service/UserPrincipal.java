7147769c2c46c9fbe0eb47df82d342539db0001e
zeledonV <valeria.zeledon@gmail.com>
2022-08-06 21:58:17 -0600

    committvaleriaZ

---
+++ src/main/java/Tienda/demo/service/UserPrincipal.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.demo.service;
+import Tienda.demo.entity.Persona;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private Persona persona;
+
+    public UserPrincipal(Persona persona) {
+        this.persona = persona;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        //Extraxt list of permissions (name)
+        this.persona.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorities.add(authority);
+        });
+        // Extract list of roles (ROLE_name)
+        this.persona.getRoleList().forEach(r -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + r);
+            authorities.add(authority);
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.persona.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.persona.getNombre();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.persona.getActive() == 1;
+    }
+}
